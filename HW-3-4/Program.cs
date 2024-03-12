dd96b834ce0f4aad77c0500afa82199036ae58c3
IrtuganovDenis <157587358+IrtuganovDenis@users.noreply.github.com>
2024-03-12 21:47:49 +0300

    Program.cs

--- HW-3-4/Program.cs
+++ HW-3-4/Program.cs
@@ -1,4 +1,4 @@
-﻿using HW_3_4;
+using HW_3_4;
 using System;
 using System.Reflection;
 using System.Security.Claims;
@@ -16,11 +16,9 @@ namespace HW_3_4
             DoubleProv dp = new DoubleProv();
             IntProv ip = new IntProv();
             StrProv sp = new StrProv();
-            IPrinter[] array = new IPrinter[8];
+            IPrinter[] array = new IPrinter[4];
 
 
-            Console.WriteLine("Введите количество эелементов одномерного массива");
-            n = int.Parse(Console.ReadLine());
             Console.WriteLine("Выберите способ ввода элементов одномерного массива (1 - вручную, 0 - рандомно)");
             t = int.Parse(Console.ReadLine());
             b = t == 1 ? true : false;
@@ -29,21 +27,17 @@ namespace HW_3_4
             array[2] = new OneArray<int>(ip, b);
             array[3] = new OneArray<string>(sp, b);
 
-
-            Console.WriteLine("Введите размер двумерного массива");
-            n = int.Parse(Console.ReadLine());
-            m = int.Parse(Console.ReadLine());
             Console.WriteLine("Выберите способ ввода элементов двумерного массива (1 - вручную, 0 - рандомно)");
             t = int.Parse(Console.ReadLine());
             b = t == 1 ? true : false;
             array[0] = new TwoArray<bool>(bp, b);
-            //array[1] = new OneArray<double>(dp, b);
-            //array[2] = new OneArray<int>(ip, b);
-            //array[3] = new OneArray<string>(sp, b);
+            array[1] = new TwoArray<double>(dp, b);
+            array[2] = new TwoArray<int>(ip, b);
+            array[3] = new TwoArray<string>(sp, b);
 
             foreach (IPrinter a in array)
             {
-                array.Print();
+                a.Print();
 
                 Console.WriteLine();
             }
